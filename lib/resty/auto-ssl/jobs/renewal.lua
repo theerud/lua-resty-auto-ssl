8c483cecb3bb57f6a724d21360276f19eaa746f2
Theerud Lawtrakul <theerud@readyplanet.com>
2020-02-21 10:33:21 +0700

    Remove no longer used function.

--- lib/resty/auto-ssl/jobs/renewal.lua
+++ lib/resty/auto-ssl/jobs/renewal.lua
@@ -191,14 +191,6 @@ local function renew_check_cert(auto_ssl_instance, storage, domain)
   renew_check_cert_unlock(domain, storage, local_lock, distributed_lock_value)
 end
 
-local function shuffle(tbl)
-  for i = #tbl, 1, -1 do
-    local rand = math.random(i)
-    tbl[i], tbl[rand] = tbl[rand], tbl[i]
-  end
-  return tbl
-end
-
 local function renew_all_domains(auto_ssl_instance)
   -- Loop through all known domains and check to see if they should be renewed.
   local storage = auto_ssl_instance.storage
